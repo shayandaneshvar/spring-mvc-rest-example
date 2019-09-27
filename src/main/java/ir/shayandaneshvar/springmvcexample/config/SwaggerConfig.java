0bdf83838820ca0674129073f666a4c7bbbbab82
shayandaneshvar <daneshvarshayan@gmail.com>
2019-09-27 22:52:02 +0330

    added support for swagger and swagger ui

---
+++ src/main/java/ir/shayandaneshvar/springmvcexample/config/SwaggerConfig.java
@@ -0,0 +1,29 @@
+package ir.shayandaneshvar.springmvcexample.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig /*extends WebMvcConfigurationSupport*/ {
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2).select().apis(
+                RequestHandlerSelectors.any()).paths(PathSelectors.any()).build()
+                .pathMapping("/");
+    }
+
+    //required for non spring-boot applications in order to configure swagger-ui
+//    @Override
+//    protected void addResourceHandlers(ResourceHandlerRegistry registry) {
+//        registry.addResourceHandler("swagger-ui.html").addResourceLocations(
+//                "classpath:/META-INF/resources/");
+//        registry.addResourceHandler("/webjars/**").addResourceLocations("classpath" +
+//                ":/META-INF/resources/webjars/");
+//    }
+}
